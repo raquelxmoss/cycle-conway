b6d9960a6837d8d6aadadad82400e6f6692bc571
Raquel Moss <raquelxmoss@gmail.com>
2016-02-06 19:30:47 +1300

    Start button seeds the grid

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import { Observable } from 'rx';
-import { div } from '@cycle/dom';
+import { div, button } from '@cycle/dom';
 import _ from 'lodash';
 
 const grid = _.range(0, 10).map(function(){
-  return _.range(0, 10).map(() => '')
+  return _.range(0, 10).map(() => ({}))
 })
+
+function seedGrid(grid, probability = 0) {
+  const seededGrid = grid.map(row => {
+    return row.map(cell => {
+      let rand = Math.random()
+
+      cell.alive = probability > rand
+
+      return cell
+    })
+  })
+
+  return seededGrid
+}
 
 function renderCell (cell) {
   return (
-      div('.cell', cell)
+    div(`.cell ${cell.alive ? '.alive' : ''}`)
   )
 }
 
@@ -24,9 +38,41 @@ function renderGrid (grid) {
   )
 }
 
+function startGame (e) {
+  return function seedGame (state) {
+    const seededGrid = seedGrid(grid, 0.2)
+
+    return Object.assign({}, state, {grid: seededGrid})
+  }
+}
+
+const initialState = {
+  grid
+}
+
 export default function App ({DOM}) {
+  const startClick$ = DOM
+    .select('.start')
+    .events('click')
+
+  const startGame$ = startClick$
+    .map(e => startGame(e))
+
+  const reducers$ = Observable.merge(
+    startGame$
+  )
+
+  const state$ = reducers$
+    .startWith(initialState)
+    .scan((state, reducer) => reducer(state))
+    .do(console.log.bind(console, 'state'))
+
   return {
-    DOM: Observable.just(div(renderGrid(grid)))
+    DOM: state$.map(({grid}) => (div([
+        renderGrid(grid),
+        button('.start', 'Start')
+      ])
+    ))
   };
 }
 
@@ -35,8 +81,8 @@ export default function App ({DOM}) {
 // The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells,
 
 //  1. Render a grid [x]
-//  2. Render a start button
-//  3. Seed a grid randomly
+//  2. Render a start button [x]
+//  3. Seed a grid randomly when start button is clicked [x]
 //  4. Implement each rule by TDD
 
 // each of which is in one of two possible states, alive or dead.
